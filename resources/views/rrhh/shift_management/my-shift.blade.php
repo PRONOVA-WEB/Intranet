f207a062e1b4e86cb215723326880d19aacde061
PRONOVA-WEB <adiaz@pronova.cl>
2022-04-06 12:28:09 -0400

    mejora de diseño miturno M. Turnos

--- resources/views/rrhh/shift_management/my-shift.blade.php
+++ resources/views/rrhh/shift_management/my-shift.blade.php
@@ -2,166 +2,16 @@
 
 @section('title', 'Gestion de Turnos')
 
-@push('css')
-<link rel="stylesheet" href="https://cdn.datatables.net/1.11.4/css/dataTables.bootstrap4.min.css">
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/2.2.2/css/buttons.dataTables.min.css">
-@endpush
+@section('custom_css')
+<style>
+.table td, .table th {
+    vertical-align: middle !important;
+}
+</style>
+@endsection
 
 @section('content')
-    <style type="text/css">
-        .scroll {
-            max-height: 200px;
-            overflow-y: auto;
-        }
 
-    </style>
-    <!--Menu de Filtros  -->
-
-    <style type="text/css">
-        :root {
-            font-size: 16px;
-        }
-
-        .table {
-            white-space: nowrap;
-        }
-
-        .table thead th {
-            text-align: center;
-            vertical-align: middle;
-            border-bottom: none;
-            border: none;
-        }
-
-        .brless {
-            /* border-right: solid 1px transparent !important; */
-        }
-
-        .bless {
-            border: none !important;
-        }
-
-        .br {
-            border-right: solid 1px #454d55 !important;
-        }
-
-        .dia {
-            opacity: 0.8;
-        }
-
-        .day {
-            background-color: white;
-            text-align: center;
-        }
-
-        .night {
-            background-color: rgba(0, 0, 0, 0.2);
-            text-align: center;
-            border-right-color: black !important;
-        }
-
-        .calendar-day {
-            font-size: 2rem;
-            text-align: center;
-            padding: 0 !important;
-        }
-
-        .table th,
-        .table td {
-            padding: 0.5rem !important;
-        }
-
-        .borderBottom {
-            border-bottom: solid 2px #454d55 !important;
-        }
-
-        .bbd {
-            border-top: none;
-            border-left: none;
-            border-right: none;
-
-        }
-
-        .bbn {
-            border-top: none !important;
-            border-left: none;
-            border-right: solid 1px #454d55;
-
-        }
-
-        .bg-red {
-            background-color: #ff5133;
-        }
-
-        .bg-green {
-            background-color: #00e63d;
-        }
-
-        .bg-purple {
-            background-color: #d57aff;
-        }
-
-        .bg-red,
-        .bg-green,
-        .bg-purple {
-            color: white;
-        }
-
-
-        .turn-selected {
-            background: #ff0000;
-            color: #fff;
-            padding: 3px 15px;
-            border-radius: 50%;
-
-        }
-
-        .only-icon {
-            background-color: Transparent;
-            background-repeat: no-repeat;
-            border: none;
-            cursor: pointer;
-            overflow: hidden;
-            outline: none;
-        }
-
-        .btnShiftDay:hover {
-            opacity: 0.5;
-            filter: alpha(opacity=50);
-        }
-
-        .btn-light {
-            border: 1px solid #ced4da;
-        }
-
-        td {
-            overflow: hidden;
-        }
-
-        .cellbutton {
-            width: 30px;
-            font-size: 13px;
-        }
-
-        .btn-full {
-            display: block;
-            width: 100%;
-            height: 100%;
-            margin: -1000px;
-            padding: 1000px;
-            font-weight: bold;
-        }
-
-        .deleteButton {
-            color: red;
-        }
-
-        .deleteButton:hover {
-            opacity: 0.5;
-            filter: alpha(opacity=50);
-        }
-
-    </style>
     @include('rrhh.shift_management.tabs', ['actuallyMenu' => 'MyShiftTab'])
 
     <div>
@@ -189,8 +39,8 @@
                                     te asigno el día {{ $day->day }}
                                     <b style="background-color: yellow;color:gray"> {{ $day->working_day }} </b> .
                                     @if (App\Models\Rrhh\ShiftUserDay::where('id', '<>', $day->id)->where('day', $day->day)->whereHas('ShiftUser', function ($q) {
-        $q->where('user_id', Auth::user()->id);
-    })->get())
+                                            $q->where('user_id', Auth::user()->id);
+                                        })->get())
                                         @php
 
                                             $dayInTheSame = App\Models\Rrhh\ShiftUserDay::where('day', $day->day)
@@ -251,54 +101,31 @@
                     <label for="for_submit">&nbsp;</label>
                     <button type="submit" class="btn btn-primary form-control">Filtrar</button>
                 </div>
-
             </div>
-
         </form>
         <hr>
+        <h4>
+            @foreach ($months as $index => $month)
+            {{ $index == $actuallyMonth ? $month : '' }}
+            @endforeach
+
+            {{ $actuallyYear }}
+            -
+            {{ $actuallyShift->name }}
+        </h4>
         <table class="table table-sm table-bordered datatable">
-            <thead class="card-header">
-                <tr>
-                    <th class="calendar-day" colspan="{{ $days }}">
-
-                        <a href="{{ route('rrhh.shiftManag.myshift', ['turnFilter' => $actuallyShift->id,'monthYearFilter' => $actuallyYear . '-' . intval($actuallyMonth) - 1]) }}"
-                            class="btn btn-sm btn-secondary btn-icon-split">
-                            <span class="icon text-white-50">
-                                <i class="fas fa-arrow-left"></i>
-                            </span>
-                            <span class="text">Anterior</span>
-                        </a>
-
-                        @foreach ($months as $index => $month)
-                            {{ $index == $actuallyMonth ? $month : '' }}
-                        @endforeach
-
-                        {{ $actuallyYear }}
-                        -
-                        {{ $actuallyShift->name }}
-
-                        <a href="{{ route('rrhh.shiftManag.myshift', ['turnFilter' => $actuallyShift->id,'monthYearFilter' => $actuallyYear . '-' . intval($actuallyMonth) + 1]) }}"
-                            class="btn btn-sm btn-secondary btn-icon-split">
-                            <span class="text">Siguiente</span>
-                            <span class="icon text-white-50">
-                                <i class="fas fa-arrow-right"></i>
-                            </span>
-                        </a>
-                    </th>
-                </tr>
-                <tr>
+            <thead>
+                <tr class="bg-gray-600 text-gray-100 text-center">
                     <th>Personal</th>
                     @for ($i = 1; $i <= $days; $i++)
                         @php
                             $dateFiltered = \Carbon\Carbon::createFromFormat('Y-m-d', $actuallyYear . '-' . $actuallyMonth . '-' . $i, 'Europe/London');
                         @endphp
-                        <th class="brless dia"
-                            style="color:{{ $dateFiltered->isWeekend()? 'red': (sizeof($holidays->where('date', $actuallyYear . '-' . $actuallyMonth . '-' . $i)) > 0? 'red': 'white') }}">
-                            <p style="font-size: 8px">{{ $i }}</p>
+                        <th style="color:{{ $dateFiltered->isWeekend()? 'red': (sizeof($holidays->where('date', $actuallyYear . '-' . $actuallyMonth . '-' . $i)) > 0? 'red': 'white') }}">
+                            <p style="font-size: 10px">{{ $i }}</p>
                         </th>
-                        <!-- <th class="brless dia">🌞</th> -->
-                        <!-- <th class="noche">🌒</th> -->
                     @endfor
+                    <th><i class="fas fa-trash"></i></th>
                 </tr>
             </thead>
             <tbody>
@@ -311,7 +138,7 @@
     </div>
 
 @endsection
-@livewire("rrhh.modal-edit-shift-user-day")
+{{-- @livewire("rrhh.modal-edit-shift-user-day") --}}
 
 @section('custom_js')
 
