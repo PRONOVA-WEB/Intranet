709acd7f4fdcc3935b0e69281061e1b37f3bd3a7
PRONOVA-WEB <adiaz@pronova.cl>
2022-05-02 10:23:34 -0400

    config livewire assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ settings('site.title') }} | @yield('title')</title>
     <meta content="Pronova" name="author" />
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
     <!-- Scripts -->
     <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
     <script src="{{ asset('js/custom.js') }}"></script>
